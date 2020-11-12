3ab5c6a9a741e4aa28985dbfd3cae474c4380234
kofi.baafi <kbaafi@gmail.com>
2020-11-12 12:35:32 +0200

    Modify buildspec.yaml

--- src/main/java/fi/op/demo/controller/HealthCheck.java
+++ src/main/java/fi/op/demo/controller/HealthCheck.java
@@ -1,17 +1,18 @@
 package fi.op.demo.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value="/demo")
-public class Demo {
-    @GetMapping(value="/data")
-    public String getDemoData() {
-        return "Demo Data";
+@RequestMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+public class HealthCheck {
+    @GetMapping(value = "/health")
+    @ResponseStatus(HttpStatus.OK)
+    public String getHealth() {
+        return "Health check invoked";
     }
 }
-
